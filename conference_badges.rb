fcff03c4fcf6d8e44331519098fd1643c088f86b
Vicki Hamm <vicki.cello@gmail.com>
2018-06-19 19:13:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,9 @@ def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badge_maker(attendees)
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index do |name|
+    "Hello, name! You'll be assigned to room index!"
+  end
+  
